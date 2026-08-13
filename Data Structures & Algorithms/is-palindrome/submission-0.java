0ede8259f51017f689cbea1b9216963f4c106b9b
Sayed Mohamed Saqib <121971986+SayedMohamedSaqib@users.noreply.github.com>
2026-08-13 11:08:28 +0530

    Update: is-palindrome - submission-0

--- Data Structures & Algorithms/is-palindrome/submission-0.java
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -2,21 +2,26 @@ class Solution {
     public boolean isPalindrome(String s) {
         int left = 0;
         int right = s.length() - 1;
-        
-        while(left < right){
 
-            while(left<right  && !Character.isLetterOrDigit(s.charAt(left))) {
+        while (left < right) {
+
+            // Skip non-alphanumeric characters from the left
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
                 left++;
             }
 
-            while(left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+            // Skip non-alphanumeric characters from the right
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
                 right--;
             }
 
-            if(Character.toLowerCase(s.charAt(left)) 
-            != Character.toLowerCase(s.charAt(right))){
+            // Compare characters ignoring case
+            if (Character.toLowerCase(s.charAt(left)) !=
+                Character.toLowerCase(s.charAt(right))) {
                 return false;
             }
+
+            // Move inward after successful comparison
             left++;
             right--;
         }
